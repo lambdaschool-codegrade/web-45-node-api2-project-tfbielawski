ff66b6f6a9ba9347e9f327735a050366e1fb4947
Tom Bielawski <tfbielawski@protonmail.com>
2021-08-31 14:51:42 -0400

    initial commit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,17 @@
 // implement your posts router here
+const express = require("express");
+const Post = require("./posts-model");
+const router = express.Router();
+
+//GET    | /api/posts
+router.get("./api/posts", (req, res) => {
+    Post.find()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            console.log("GET /API/POSTS", error)
+            res.status(500).json({  message: "The posts information could not be retrieved"})
+        })
+})
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,14 @@
 // implement your server here
+const express = require("express");
 // require your posts router and connect it here
+const server = express();
+server.use(express.json());
+
+
+//Landing page
+server.get("/", (req,res) => {
+    res.send(`<h2> API2 </h2>`)
+})
+
+//Export the server
+module.exports = server;
